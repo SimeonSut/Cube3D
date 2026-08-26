b2e601a328cea3838304f1f27ec98bacf4b05be7
Chetsada Samakkasetkan <freesamakkasetkan@hotmail.com>
2026-08-26 18:21:56 +0200

    mouse rotate in progress

--- src/render/mouse_rotate.c
+++ src/render/mouse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: csamakka <csamakka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 15:01:29 by csamakka          #+#    #+#             */
-/*   Updated: 2026/08/26 16:27:21 by csamakka         ###   ########.fr       */
+/*   Updated: 2026/08/26 18:20:54 by csamakka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,22 @@ int	mouse_rotate(int x, int y, void *param)
 	t_data *data;
 	double	old_dir_x;
 	double 	old_plane_x;
-	double	camera_x;
+	double	rot_speed;
+	int		delta_x;
 	
+	delta_x = (x - (1920 / 2)) / 3;
+	rot_speed = 0.001;
 	data = param;
 	old_dir_x = data->player->dir_x;
 	old_plane_x = data->player->plane_x;
-	camera_x = 2.0 * x / 1920 -1;
-	if (x >= 0 && x < (1920 - 1) / 2)
-	{
-		data->player->dir_x = data->player->dir_x * cos(0.03 * camera_x) - data->player->dir_y * sin(0.03 * camera_x);
-		data->player->dir_y = old_dir_x * sin(0.03 * camera_x) + data->player->dir_y * cos(0.03 * camera_x);
-		data->player->plane_x = data->player->plane_x * cos(0.03 * camera_x) - data->player->plane_y * sin(0.03 * camera_x);
-		data->player->plane_y = old_plane_x * sin(0.03 * camera_x) + data->player->plane_y * cos(0.03 * camera_x);
-	}
-	else if ( x >= 0 && x > (1920 - 1) / 2)
-	{
-		data->player->dir_x = data->player->dir_x * cos(0.03 * camera_x) - data->player->dir_y * sin(0.03 * camera_x);
-		data->player->dir_y = old_dir_x * sin(0.03 * camera_x) + data->player->dir_y * cos(0.03 * camera_x);
-		data->player->plane_x = data->player->plane_x * cos(0.03 * camera_x) - data->player->plane_y * sin(0.03 * camera_x);
-		data->player->plane_y = old_plane_x * sin(0.03 * camera_x) + data->player->plane_y * cos(0.03 * camera_x);
-	}
-	(void)y;
+	
+	data->player->dir_x = data->player->dir_x * cos(rot_speed * delta_x) - data->player->dir_y * sin(rot_speed * delta_x);
+	data->player->dir_y = old_dir_x * sin(rot_speed * delta_x) + data->player->dir_y * cos(rot_speed * delta_x);
+	
+	data->player->plane_x = data->player->plane_x * cos(rot_speed * delta_x) - data->player->plane_y * sin(rot_speed * delta_x);
+	data->player->plane_y = old_plane_x * sin(rot_speed * delta_x) + data->player->plane_y * cos(rot_speed * delta_x);
 	map_d_render(data);
+	
+	(void)y;
 	return (0);
 }
